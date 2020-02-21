551a3193cbedb4a2a6d52ccc604ea987c653e907
Richard NSHIMYUMUKIZA <ricahcyuzuzo@gmail.com>
2020-02-21 07:55:11 +0200

    Ft sign In

---
+++ src/home/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SignUn from "./Signup.component";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <SignUn />
+    </div>
+  );
+}
+
+export default App;
